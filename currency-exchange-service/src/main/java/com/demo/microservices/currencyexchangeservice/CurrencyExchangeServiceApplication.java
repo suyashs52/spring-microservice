0ac318bdd37d759c7ca296ea22575bbc81bf4951
suyash kumar singh <suyashs52@gmail.com>
2022-11-30 03:20:32 +0530

    docker commit of microservices

--- currency-exchange-service/src/main/java/com/demo/microservices/currencyexchangeservice/CurrencyExchangeServiceApplication.java
+++ currency-exchange-service/src/main/java/com/demo/microservices/currencyexchangeservice/CurrencyExchangeServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CurrencyExchangeServiceApplication {
 	//to run on port 8001, use create run configuration, in vm param add : -DServer.port=8001
+	//after docker addition > run as > maven build
 	public static void main(String[] args) {
 		SpringApplication.run(CurrencyExchangeServiceApplication.class, args);
 	}
--- currency-exchange-service/src/main/java/com/demo/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/demo/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -2,6 +2,8 @@ package com.demo.microservices.currencyexchangeservice.controller;
 
 import com.demo.microservices.currencyexchangeservice.bean.CurrencyExchange;
 import com.demo.microservices.currencyexchangeservice.repository.CurrencyExchangeRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,10 +17,12 @@ public class CurrencyExchangeController {
 
     @Autowired
     private CurrencyExchangeRepository currencyExchangeRepository;
+    Logger logger= LoggerFactory.getLogger(CurrencyExchangeController.class);
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange retriveExchangeValue(@PathVariable String from, @PathVariable String to) {
         //  CurrencyExchange currencyExchange = new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(50));
+        logger.info("Retrieve exchange value called with {} to {} ",from,to);
         CurrencyExchange currencyExchange = currencyExchangeRepository.findByFromAndTo(from, to);
         if(currencyExchange==null){
             throw new RuntimeException("Unable to find data for "+from +" to "+to);
